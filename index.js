f880c7470ce45a2737a6540e6a804538e1ab78fe
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-09-12 11:44:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+  
+
+async function foo(){
+    var res = await fetch("https://api.chucknorris.io/jokes/random");
+  var res1= await res.json();
+  console.log(res1);
+  var div=document.createElement("div");
+  
+    try{
+        for(let i in res1){
+            div.innerHTML=`<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+    <h5 class="card-title">${res1[i]}</h5>
+    </div>
+  </div>`
+        document.body.append(div)
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+foo();
